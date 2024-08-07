2f76dca8148e93c2739fc8b81064d1b056ad8c2a
truptilingaraju <trupti.l01@testyantra.in>
2024-08-07 14:35:36 +0530

    first commit

---
+++ src/main/java/com/ty/dev_env/dev_env/DevEnvApplication.java
@@ -0,0 +1,13 @@
+package com.ty.dev_env.dev_env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevEnvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/dev_env/dev_env/DevEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.dev_env.dev_env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevEnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
